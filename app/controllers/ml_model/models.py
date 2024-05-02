4b0ab1720baf5b34fcde6ea4404c4385fc5adbb3
kheynerz <sebastiancr44@gmail.com>
2024-05-01 21:47:45 -0600

    Cirrhosis model

--- app/controllers/ml_model/models.py
+++ app/controllers/ml_model/models.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.services.agents.feeling import recognize_feeling_from_image
+from app.services.ml_model.classificate_cirrhosis import classificate_cirrhosis
 from app.services.ml_model.predict_bitcoin import predict_bitcoin
 model_routes = Blueprint('model', __name__, url_prefix='/model')
 
@@ -14,3 +15,14 @@ def bitcoin_prediction():
     
     prediction = predict_bitcoin(data["volume"], data["market_cap"])
     return jsonify(prediction)
+
+@model_routes.route('/cirrhosis', methods=['POST'])
+def cirrhosis_classification():
+    required_values = ['cholesterol', 'albumin', 'copper', 'alk_phos', 'tryglicerides', 'platelets', 'prothrombin']
+    data = request.json
+    for value in required_values:
+        if value not in data:
+            return jsonify({'error': f'"{value}"  is required in body'}), 400
+
+    classification = classificate_cirrhosis(data)
+    return jsonify(classification)
---
+++ app/services/ml_model/classificate_cirrhosis.py
@@ -0,0 +1,17 @@
+import joblib
+import numpy as np
+model = joblib.load('app\ml_model\model_cirrhosis.pkl')
+
+def classificate_cirrhosis(data):
+    cholesterol = data['cholesterol']
+    albumin = data['albumin']
+    copper = data['copper']
+    alk_phos = data['alk_phos']
+    tryglicerides = data['tryglicerides']
+    platelets = data['platelets']
+    prothrombin = data['prothrombin']
+    test_data = np.array([[cholesterol, albumin, copper, alk_phos, tryglicerides, platelets, prothrombin]])
+
+    classification = model.predict(test_data)[0]
+
+    return {"prediction" : str(classification)}
