5afd03e6cdbb49f02405b7a2b674068643a42f24
Ronald115 <ronaldarce115@gmail.com>
2024-05-07 10:50:28 -0600

    cors for localhost300

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.controllers.routes import api_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 # Register main blueprint
 app.register_blueprint(api_routes)
