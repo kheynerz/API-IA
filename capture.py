fd817254d5a09dbd82670687692ba91aa8770a3e
kheynerz <sebastiancr44@gmail.com>
2024-06-03 10:44:00 -0600

    Fix script

--- capture.py
+++ capture.py
@@ -1,45 +1,49 @@
 import cv2
+import time
 import os
-from time import sleep
+import sys
 
-def capture_photos(person_name, photos_per_person=100):
-    # Crear una carpeta para guardar las fotos de la persona si no existe
-    person_folder = f'captured_photos/{person_name}'
-    if not os.path.exists(person_folder):
-        os.makedirs(person_folder)
+emotions = ["happy", "sad", "angry", "surprised", "neutral"]
 
-    # Iniciar la captura de video desde la cámara
+def capture_photos(total_photo_count, sleep_time, emotion, initialIndex):
+    emotion_folder = f'captured_photos/{emotion}'
+    if not os.path.exists(emotion_folder):
+        os.makedirs(emotion_folder)
+
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error: No se puede acceder a la cámara.")
         return
 
-    photo_count = 0
-    max_digits = len(str(photos_per_person))
-    while photo_count < photos_per_person:
-        # Leer un frame de la cámara
+    max_digits = 3
+
+    for i in range(initialIndex * total_photo_count, initialIndex * total_photo_count + total_photo_count):
         ret, frame = cap.read()
         
         if not ret:
             print("Error: No se puede leer el frame de la cámara.")
             break
-
-        # Guardar la imagen con un nombre basado en el índice de la foto
-        photo_filename = f'{person_folder}/photo_{str(photo_count + 1).zfill(max_digits)}.jpg'
+        photo_filename = f'{emotion_folder}/{str(i + 1).zfill(max_digits)}.jpg'
         cv2.imwrite(photo_filename, frame)
         print(f'Foto guardada: {photo_filename}')
-        photo_count += 1
 
-        sleep(0.5)
+        time.sleep(sleep_time)
 
-    # Liberar la captura de video y cerrar todas las ventanas de OpenCV
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    # Nombre de la persona para la cual se van a capturar las fotos
-    person_name = input("Ingrese el nombre de la persona: ")
+    total_photo_count = 2
+    sleep_time = 0.5
 
-    # Capturar al menos 100 fotos por persona
-    capture_photos(person_name, photos_per_person=200)
+    if len(sys.argv) > 1:
+        initialIndex = int(sys.argv[1])
+    else:
+        initialIndex = 0
+    for emotion in emotions:
+        print(f"Se capturarán un total de {total_photo_count} fotos para la emoción: {emotion}")
+        input('Presiona "Enter"  para empezar...')
+        capture_photos(total_photo_count, sleep_time, emotion, initialIndex)
+        print(f"Fotos para la emoción {emotion} completadas.\n")
+
